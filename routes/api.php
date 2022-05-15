febdc2921674b3cbe1357a4b6cd16d955b040b34
Everton Paiva <everton.paiva@ufvjm.edu.br>
2022-05-15 13:13:28 -0300

    Adicionando rota de autenticação na API. Protegendo rota de listagem de usuários.

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,23 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Users;
+use App\Http\Controllers\Api\AuthController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+#### - Autenticacao - ####
+
+// Registrar novo usuario - desabilitado
+Route::post('/register', [AuthController::class, 'register']);
+
+// Login de usuario
+Route::post('/login', [AuthController::class, 'login']);
+
+// Recuperacao de senha
+Route::post('/login/recuperar', [AuthController::class, 'forgotPassword']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    // Obter o perfil do usuario
+    Route::get('/profile', [AuthController::class, 'profile']);
+
+    Route::get('/users', Users\Index::class)->name('api.users.index');
 });
-
-Route::get('/users', Users\Index::class)->name('api.users.index');
